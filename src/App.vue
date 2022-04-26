a9c63719c927a6907d1f1f10c190d5bfe2d3a7b3
slava.odinokov <odin.slavy@incodewetrust.ru>
2022-04-26 21:45:18 +0300

    added telegram api test

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
 
     <task-input @emit-add-task="addTask" />
 
-    <tab-nav 
-      :currentView="state.currentView" 
+    <tab-nav
+      :currentView="state.currentView"
       :taskListOverview="taskListOverview"
-      @update-current-view="setView" 
+      @update-current-view="setView"
     />
-    
-    <task-list 
+
+    <task-list
       :tasksInView="tasksInView"
       @emit-toggle-edit="toggleEdit"
       @emit-delete-task="deleteTask"
@@ -21,63 +21,89 @@
 </template>
 
 <script setup>
-import { reactive, computed } from 'vue'
-import { v4 as uuid } from 'uuid'
-import IconRegistry from './components/icon/icon-registry.vue'
-import HeaderTodo from './components/HeaderTodo.vue'
-import TabNav from './components/TabNav/TabNav.vue'
-import TaskList from './components/TaskList.vue'
-import TaskInput from './components/TaskInput.vue'
+import { reactive, computed } from "vue";
+import { v4 as uuid } from "uuid";
+import IconRegistry from "./components/icon/icon-registry.vue";
+import HeaderTodo from "./components/HeaderTodo.vue";
+import TabNav from "./components/TabNav/TabNav.vue";
+import TaskList from "./components/TaskList.vue";
+import TaskInput from "./components/TaskInput.vue";
+
+import moment from "moment";
+import format from "date-fns/format";
+
+const date = "2021-02-10T19:34:32.227Z";
+const currentDateTime = () => {
+  return moment(date).format("DD.MM.YYYY");
+};
+console.log(currentDateTime());
+
+const dateFns = format(new Date(date), "dd.MM.yyyy");
+console.log("dateFns ", dateFns);
 
 const state = reactive({
-  currentView: 'All',
-  taskList: []
-})
+  currentView: "All",
+  taskList: [],
+});
 
 const taskLists = reactive({
   all: computed(() => state.taskList),
-  current: computed(() => state.taskList.filter(item => !item.complete)),
-  completed: computed(() => state.taskList.filter(item => item.complete))
-})
+  current: computed(() => state.taskList.filter((item) => !item.complete)),
+  completed: computed(() => state.taskList.filter((item) => item.complete)),
+});
 
 const taskListOverview = reactive([
-  { name: 'All', length: computed(() => taskLists.all.length), status: true },
-  { name: 'Current', length: computed(() => taskLists.current.length), status: false },
-  { name: 'Completed', length: computed(() => taskLists.completed.length), status: false }
-])
+  { name: "All", length: computed(() => taskLists.all.length), status: true },
+  {
+    name: "Current",
+    length: computed(() => taskLists.current.length),
+    status: false,
+  },
+  {
+    name: "Completed",
+    length: computed(() => taskLists.completed.length),
+    status: false,
+  },
+]);
 
 const tasksInView = computed(() => {
-  if (state.currentView === 'Current') {
-    return taskLists.current
-  } else if (state.currentView === 'Completed') {
-    return taskLists.completed
+  if (state.currentView === "Current") {
+    return taskLists.current;
+  } else if (state.currentView === "Completed") {
+    return taskLists.completed;
   } else {
-    return taskLists.all
+    return taskLists.all;
   }
-})
+});
 
-const setView = value => {
-  state.currentView = value
-}
+const setView = (value) => {
+  state.currentView = value;
+};
 
-const addTask = newTaskInput => {
+const addTask = (newTaskInput) => {
   state.taskList.push({
     id: uuid(),
     label: newTaskInput,
     complete: false,
-    edit: false
-  })
-}
+    edit: false,
+  });
+};
 
-const deleteTask = id => {
-  const index = state.taskList.findIndex(task => task.id === id)
-  state.taskList.splice(index, 1)
-}
+const deleteTask = (id) => {
+  const index = state.taskList.findIndex((task) => task.id === id);
+  state.taskList.splice(index, 1);
+};
 
-const toggleEdit = id => {
-  const index = state.taskList.findIndex(task => task.id === id)
-  state.taskList[index].edit = !state.taskList[index].edit
-}
+const toggleEdit = (id) => {
+  const index = state.taskList.findIndex((task) => task.id === id);
+  state.taskList[index].edit = !state.taskList[index].edit;
+};
+
+// Telegram
+Telegram.WebApp.ready();
+
+const initData = Telegram.WebApp.initData || "";
+const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
 </script>
 
 <style>
