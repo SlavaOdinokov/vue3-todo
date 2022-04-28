3731eab674091abfe457c8f580ede6adfbe55324
slava.odinokov <odin.slavy@incodewetrust.ru>
2022-04-28 08:58:59 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -134,8 +134,14 @@ const toggleEdit = (id) => {
 // };
 
 onMounted(() => {
-  window.Telegram.WebApp.ready();
+  let recaptchaScript = document.createElement("script");
+  recaptchaScript.setAttribute(
+    "src",
+    "https://telegram.org/js/telegram-web-app.js"
+  );
+  document.head.appendChild(recaptchaScript);
 
+  Telegram.WebApp.ready();
   const initData = Telegram.WebApp.initData || "";
   const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
 
